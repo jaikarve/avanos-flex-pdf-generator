1ad37f76acbd563641f53cc22c8914b8752f6e77
Jai Karve 2 <jai.karve@gmail.com>
2019-02-07 14:47:42 -0800

    Updating code

--- index.js
+++ index.js
@@ -13,9 +13,12 @@ const PDFDocument = require('pdfkit');
 const ms = require('memory-streams');
 const b64 = require('base-64');
 const axios = require('axios');
+const fs = require('fs');
+const {Base64Encode} = require('base64-stream');
 
 
-function sendPDF(doc, context, complete) {
+
+function sendPDF(base64Doc, context, complete) {
     authenticateRedox()
     .then((authResponse) => {
 
@@ -25,81 +28,82 @@ function sendPDF(doc, context, complete) {
                 "Authorization":"Bearer " + authResponse.data.accessToken
             }
         }
-
-        let responseStream =  new ms.WritableStream();
-        let stream = doc.pipe(responseStream);
-        stream.on('finish', function() {
-            let pdfBytes = stream.toString();
-            var encoded = Buffer.from(pdfBytes).toString('base64')
-
-            //console.log("Encoded base64: " + encoded);
-
-            console.log("PDF Bytes length: " + pdfBytes.length);
-                
-            let postData = {
-                "Meta":{
-                    "DataModel":"Media",
-                    "EventType":"New"
+     
+        let postData = {
+            "Meta":{
+                "DataModel":"Media",
+                "EventType":"New"
+            },
+            "Patient":{
+                "Identifiers":[
+                    {
+                        "ID":"0000000001",
+                        "IDType":"MR"
+                    }
+                ]
+            },
+            "Media":{
+                "FileType":"PDF",
+                "FileName":"Sample",
+                "FileContents":base64Doc,
+                "DocumentType": "Feeding Tube Placement",
+                "DocumentID":"b124567",
+                "Provider":{
+                    "ID":"8675309"
                 },
-                "Patient":{
-                    "Identifiers":[
-                        {
-                            "ID":"0000000001",
-                            "IDType":"MR"
-                        }
-                    ]
-                },
-                "Media":{
-                    "FileType":"PDF",
-                    "FileName":"Sample",
-                    "FileContents":encoded,
-                    "DocumentType": "Feeding Tube Placement",
-                        "DocumentID":"b124567",
-                        "Provider":{
-                            "ID":"8675309"
-                        },
-                    "Availability":"Available"
+                "Availability":"Available"
+            }
+        };
+
+        axios.post("https://api.redoxengine.com/endpoint",
+            postData, axiosConfig)
+        .then((responseData) => {
+            //console.dir(responseData);
+            var rawDataString = Buffer.from(base64Doc, 'base64');
+
+            let kvFileConfig = {
+                headers: {
+                    "Content-Type":"application/json",
+                    "Authorization": "Basic a2lkX0h5SXNJMWtnRTpmY2Y0ZDQyZGI2MjY0ZTA1YWI4YTQwYzU4ZDg3NjE1ZA==",
+                    "X-Kinvey-Content-Type":"application/pdf"
                 }
-            };
+            }
 
-            axios.post("https://api.redoxengine.com/endpoint",
-                postData, axiosConfig)
-            .then((responseData) => {
+            let kvFilePostData = {
+                "_filename": "myFilename.pdf",
+                "myProperty": "some metadata",
+                "someOtherProperty": "some more metadata",
+                "mimeType":"application/pdf"
+            }
 
-                let kvFileConfig = {
+            axios.post("https://kvy-us2-baas.kinvey.com/blob/kid_HyIsI1kgE",
+                kvFilePostData, kvFileConfig)
+            .then((resp) => {
+                let fileId = resp.data._id;
+                let gcsFileConfig = {
                     headers: {
-                        "Content-Type":"application/json",
-                        "Authorization": "Basic a2lkX0h5SXNJMWtnRTpmY2Y0ZDQyZGI2MjY0ZTA1YWI4YTQwYzU4ZDg3NjE1ZA==",
-                        "X-Kinvey-Content-Type":"application/pdf"
+                        "Content-Length": rawDataString.length,
+                        "Content-Type":"application/pdf"
                     }
                 }
 
-                let kvFilePostData = {
-                    "_filename": "myFilename.pdf",
-                    "myProperty": "some metadata",
-                    "someOtherProperty": "some more metadata",
-                    "mimeType":"application/pdf"
-                }
-
-                axios.post("https://kvy-us2-baas.kinvey.com/blob/kid_HyIsI1kgE",
-                    kvFilePostData, kvFileConfig)
+                axios.put(resp.data._uploadURL, rawDataString, gcsFileConfig)
                 .then((resp) => {
                     //console.dir(resp);
-                    let gcsFileConfig = {
+                    
+                    let kvGetFileConfig = {
                         headers: {
-                            "Content-Length": pdfBytes.length,
-                            "Content-Type":"application/pdf"
+                            "Authorization": "Basic a2lkX0h5SXNJMWtnRTpmY2Y0ZDQyZGI2MjY0ZTA1YWI4YTQwYzU4ZDg3NjE1ZA=="
                         }
                     }
 
-                    console.log("Upload URL: " + resp.data._uploadURL);
+                    let getURI = "https://kvy-us2-baas.kinvey.com/blob/kid_HyIsI1kgE/" + fileId;
 
-                    axios.put(resp.data._uploadURL, pdfBytes, gcsFileConfig)
+                    axios.get(getURI, kvGetFileConfig)
                     .then((resp) => {
-                        console.dir(resp);
                         return complete().setBody({
                             "statusCode":200,
-                            "uga buga":true
+                            "downloadURL":resp.data._downloadURL
                         }).ok().done();
                     })
                     .catch((err) => {
@@ -112,16 +116,21 @@ function sendPDF(doc, context, complete) {
                     return complete().setBody(new Error("error")).badRequest().done();
                 })
             })
-            .catch((responseError) => {
-                console.log("Response error: ");
-                console.log(JSON.stringify(responseError.response.data.Meta));
+            .catch((err) => {
+                console.dir(err);
                 return complete().setBody(new Error("error")).badRequest().done();
             })
-        });
+        })
+        .catch((responseError) => {
+            console.dir(responseError);
+            //console.log(JSON.stringify(responseError.response));
+            return complete().setBody(new Error("error")).badRequest().done();
+        })
     })
     .catch((authError) => {
-        return complete().setBody(new Error(authError)).unauthorized().done();
-    })
+        console.log("Auth Error");
+        return complete().setBody(new Error("Auth Error")).badRequest().done();
+    });
 }
 
 async function authenticateRedox() {
@@ -156,6 +165,8 @@ sdk.service((err, flex) => {
     f.register('generate', function(context, complete, modules){
         let requestBody = context.body;
 
+        //console.dir("File upload: " + context.body.fileUpload);
+
         if(requestBody == null) {
             return complete().setBody({ "error": "must provide payload to convert"}).badRequest().done();
         }
@@ -164,51 +175,76 @@ sdk.service((err, flex) => {
             return complete().setBody({ "error": "could not instantiate pdf document"}).runtimeError().done();  
         }
         
+        
         /** process the document here  */
-        let lorem = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam in suscipit purus.  Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Vivamus nec hendrerit felis. Morbi aliquam facilisis risus eu lacinia. Sed eu leo in turpis fringilla hendrerit. Ut nec accumsan nisl.'
-        doc.fontSize(8);
-        doc.text('This text is left aligned. ' + lorem, {
-            width: 410,
-            align: 'left'
-        });
+        //let lorem = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam in suscipit purus.  Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Vivamus nec hendrerit felis. Morbi aliquam facilisis risus eu lacinia. Sed eu leo in turpis fringilla hendrerit. Ut nec accumsan nisl.'
+        
+        let finalString = ''; 
+        var stream = doc.pipe(new Base64Encode());
 
-        doc.moveDown();
-        doc.text('This text is left aligned. ' + lorem, {
-            width: 410,
-            align: 'center'
-        });
-        doc.moveDown();
-        doc.text('This text is justified. ' + lorem, {
-            width: 410,
-            align: 'justify'
-        });
-            
-        doc.rect(doc.x, 0, 410, doc.y).stroke();
-        doc.end();
-        /** end processing the document  */
+        // draw some text
 
-        sendPDF(doc, context, complete);
-
-        /*
-        authenticateRedox()
-        .then((redoxAuthResponse) => {
-            //flex.logger.info("Response is: " + JSON.stringify(redoxAuthResponse));
-            let responseStream =  new ms.WritableStream();
-            let stream = doc.pipe(responseStream);
-            stream.on('finish', function(){
-                let pdfBytes = stream.toString();
-                var encoded = Buffer.from(pdfBytes).toString('base64')
-
-                    return complete().setBody(
-                        {
-                            "PDFBytes": encoded,
-                            "accessToken":redoxAuthResponse.data.accessToken
-                        }
-                    ).ok().done();
+        doc.fontSize(25)
+            .text('NG Tube Order Placement PDF', 
+            {
+                align: "center"    
             });
-        })
-        .catch((authError) => {
-            return complete().setBody(new Error(authError)).unauthorized().done();
-        });*/
+        doc.moveDown();
+        doc.fontSize(25)
+            .text(requestBody.patientName,
+            {
+                align: "center"
+            });
+        doc.moveDown();
+        doc.fontSize(25)
+            .text("Date Of Birth: " + requestBody.patientDOB,
+            {
+                align: "center"
+            });
+        doc.moveDown();
+        doc.fontSize(16)
+            .text("Placement Date: " + requestBody.placementDate,
+            {
+                align: "left"
+            });
+        doc.moveDown();
+        doc.text("Placement Time: " + requestBody.placementTime);
+        doc.moveDown();
+        doc.text("Placement Site: " + requestBody.site);
+        doc.moveDown();
+        doc.text("Tube Length: " + requestBody.tubeLength);
+        doc.moveDown();
+        doc.text("French Size: " + requestBody.frenchSize);
+        doc.moveDown();
+        doc.text("Order ID: " + requestBody.orderId);
+        doc.moveDown();
+        doc.text("Lot Size: " + requestBody.lotNumber);
+        doc.moveDown();
+        doc.addPage();
+        doc.fontSize(25)
+            .text("Placement Images",
+            {
+                align: "center"
+            });
+        doc.moveDown();
+
+        doc.image(context.body.imageUrl1);
+
+        doc.moveDown();
+
+        doc.image(context.body.imageUrl2);
+   
+        // end and display the document in the iframe to the right
+        doc.end();
+
+        stream.on('data', function(chunk) {
+            finalString += chunk;
+        });
+
+        stream.on('end', function() {
+            //let pdfBytes = stream.toString();
+
+            sendPDF(finalString, context, complete);
+        });
     });
 });
